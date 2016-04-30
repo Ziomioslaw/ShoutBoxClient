111b3bdb553cbcd0508d597e50338be57447c6f2
Ziomioslaw <ziomioslaw@wp.pl>
2016-04-30 18:38:58 +0200

    Fixing problem with missplaced code

--- src/javascript/actions/engines.js
+++ src/javascript/actions/engines.js
@@ -1,25 +1,28 @@
-context.BeforeSubmitManager.register(function(shout, event) {
-    if (!shout.startsWith('/')) {
+context.BeforeSubmitManager.register((function(){
+    var engines = [
+        { nick: 'google', link: 'https://www.google.com/search?q=' },
+        { nick: 'wiki', link: 'https://pl.wikipedia.org/w/index.php?search=' },
+        { nick: 'duck', link: 'https://duckduckgo.com/?q=' },
+        { nick: 'gimage', link: 'https://www.google.pl/search?tbm=isch&q=' },
+        { nick: 'dimage', link: 'https://duckduckgo.com/?iax=1&ia=images&q=' }
+    ];
+
+    engines.forEach(function(engine) {
+        engine.regex = new RegExp('\\[' + engine.nick + ':([^\\]]+)\\]');
+    });
+
+    return function(shout, event) {
+        var match;
+        engines.forEach(function(engine) {
+            match = shout.match(engine.regex);
+            if (match) {
+                event.stop = true;
+                shout = shout.replace(
+                    '[' + engine.nick + ':' + match[1] + ']',
+                    '[url=' + engine.link + encodeURIComponent(match[1]) + ']' + match[1] + '[/url]');
+            }
+        });
+
         return shout;
-    }
-
-    switch(true) {
-        case (shout.startsWith('/g ')):
-            return toUrl(shout.substring(3), 'https://www.google.com/search?q=');
-        case (shout.startsWith('/gi ')):
-            return toUrl(shout.substring(4), 'https://www.google.pl/search?tbm=isch&q=');
-        case (shout.startsWith('/ddg ')):
-            return toUrl(shout.substring(5), 'https://duckduckgo.com/?q=');
-        case (shout.startsWith('/ddgi ')):
-            return toUrl(shout.substring(6), 'https://duckduckgo.com/?iax=1&ia=images&q=');
-        case (shout.startsWith('/w ')):
-            return toUrl(shout.substring(3), 'https://pl.wikipedia.org/w/index.php?search=');
-        default:
-            return shout;
-    }
-
-    function toUrl(question, url) {
-        event.stop = true;
-        return url + encodeURIComponent(question);
-    }
-});
+    };
+})());
--- src/javascript/core/beforeSubmitManager.js
+++ src/javascript/core/beforeSubmitManager.js
@@ -1,28 +1,21 @@
-context.BeforeSubmitManager.register((function(){
-    var engines = [
-        { nick: 'google', link: 'https://www.google.com/search?q=' },
-        { nick: 'wiki', link: 'https://pl.wikipedia.org/w/index.php?search=' },
-        { nick: 'duck', link: 'https://duckduckgo.com/?q=' },
-        { nick: 'gimage', link: 'https://www.google.pl/search?tbm=isch&q=' },
-        { nick: 'dimage', link: 'https://duckduckgo.com/?iax=1&ia=images&q=' }
-    ];
+context.BeforeSubmitManager = (function() {
+    var registred = [];
 
-    engines.forEach(function(engine) {
-        engine.regex = new RegExp('\\[' + engine.nick + ':([^\\]]+)\\]');
-    });
+    return {
+        register: function(f) {
+            registred.push(f);
+        },
+        call: function(event) {
+            var origin = event.origin;
+            for (var i = 0, max = registred.length; i < max; i++) {
+                event.origin = origin;
+                event.message = registred[i](event.message, event);
+                event.wasChanged = event.origin !== event.message;
 
-    return function(shout, event) {
-        var match;
-        engines.forEach(function(engine) {
-            match = shout.match(engine.regex);
-            if (match) {
-                event.stop = true;
-                shout = shout.replace(
-                    '[' + engine.nick + ':' + match[1] + ']',
-                    '[url=' + engine.link + encodeURIComponent(match[1]) + ']' + match[1] + '[/url]');
+                if (event.stop || event.cancel) {
+                    return;
+                }
             }
-        });
-
-        return shout;
+        }
     };
-})());
+})();
